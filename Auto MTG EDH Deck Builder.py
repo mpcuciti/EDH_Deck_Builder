5b244890560076f4c21dfd25a4d7add1ef6924f2
Michael Cuciti <michael.p.cuciti@gmail.com>
2021-09-26 17:28:57 -0400

    IT WORKS!! (most of the time)

--- Auto MTG EDH Deck Builder.py
+++ Auto MTG EDH Deck Builder.py
@@ -1,5 +1,7 @@
-from AutoEDHFunctionsandClasses import * 
+from random import seed
+from AutoEDHFunctionsandClasses import *
 from pprint import pprint
+import psycopg2
 
 #Here there be PsuedoCode
 # 1. Is the given card a valid commander?
@@ -10,54 +12,35 @@ from pprint import pprint
 # 5. The total size of the desired deck list 100 less 15 top cards less 1 commander less the number of lands (lands also need to be proportional based on color identity)
 # 6. 
 
-# there will eventually be a prompt here
-seed_card_url = 'https://edhrec.com/cards/sen-triplets'
-#seed_card_url = 'https://edhrec.com/cards/izzet-signet'
-seed_card = mtgcard(seed_card_url)
+#yes my DB credentials are in here
+# no it's not permanent (just for testing)
+# dont worry about it
+conn = psycopg2.connect(
+    host="postgresql.cuciti.net",
+    database="mtgcard_db",
+    user="mtgcard_db_user",
+    password="jVvWcapRwg5j74"
+)
+
+seed_card_url = input('whats the edhrec url of your seed card?')
+seed_card = mtgcard(seed_card_url, conn)
 print('Seed Card is', seed_card.name)
 
 if seed_card.legal_commander == True:
-    print(seed_card.name, 'is a legal commander')
-    yesno = None
-    index = 0
-    while yesno != 'y':
-        if yesno == None:
-            yesno = input('Do you want this card to be your commander? (y/n) ')
-            if yesno == 'y':
-                    commander = mtgcard(seed_card.url, commander=True)
-        elif yesno == 'n':
-            if index < len(seed_card.top_commanders):
-                temp_card = mtgcard(seed_card.top_commanders[index]['url'])
-                pprint(temp_card.name)
-                yesno = input('Do you want this card to be your commander? (y/n) ')
-                if yesno == 'y':
-                    commander = mtgcard(temp_card.url, commander=True)
-                index += 1
-            elif index >= len(seed_card.top_commanders):
-                yesno = 'y'
-                print('No Legal Commanders Remaining')
-        else:
-            print('Invalid Input')
-else:
-    print(seed_card.name, 'is not a legal commander')
-    yesno = 'n'
-    index = 0
-    while yesno != 'y':
-        if yesno == 'n':
-            if index < len(seed_card.top_commanders):
-                temp_card = mtgcard(seed_card.top_commanders[index]['url'])
-                pprint(temp_card.name)
-                yesno = input('Do you want this card to be your commander? (y/n) ')
-                if yesno == 'y':
-                    commander = temp_card
-                index += 1
-            elif index >= len(seed_card.top_commanders):
-                yesno = 'y'
-                print('No Legal Commanders Remaining')
-        else:
-            print('Invalid Input')
+    print('Commander is', seed_card.name)
+    deck = mtgdeck(seed_card, conn=conn)
+elif seed_card.legal_commander != True:
+    for card in seed_card.related_cards.values():
+        temp_card = mtgcard(card['url'], conn)
+        if temp_card.legal_commander == True:
+            print('Commander is', temp_card.name)
+            deck = mtgdeck(seed_card, conn=conn)
 
-deck = mtgdeck(commander)
-deck.add_top_commander_cards(3)
-
-pprint(deck.deck_list.keys())
+deck.calculate_average_mana()
+for x in range (100 - deck.land_target):
+    deck.add_synergistic_card()
+deck.calculate_average_mana()
+deck.landfall()
+for x in range (100 - deck.count_cards()):
+    deck.add_synergistic_card(['Creature', 'Sorcery', 'Instant', 'Enchantment', 'Artifact', 'Planeswalker'])
+pprint(deck.print_deck_list())
